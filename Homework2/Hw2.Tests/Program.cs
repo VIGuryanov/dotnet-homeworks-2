ac21224a398002dbcb4e821a6498e34414eae46a
Guryanov Vadim <vadim.guryanov.2004@mail.ru>
2022-09-17 23:08:53 +0300

    update

--- Homework2/Hw2.Tests/Program.cs
+++ Homework2/Hw2.Tests/Program.cs
@@ -1,12 +1,6 @@
 ﻿using Hw2;
 
-public class Program
-{ 
-    public static void Main(string[] args)
-    {
-        Parser.ParseCalcArguments(args, out double val1, out CalculatorOperation operation, out double val2);
+Parser.ParseCalcArguments(args, out double val1, out CalculatorOperation operation, out double val2);
 
-        var result = Calculator.Calculate(val1, operation, val2);
-        Console.WriteLine(result);
-    }
-}
+var result = Calculator.Calculate(val1, operation, val2);
+Console.WriteLine(result);
