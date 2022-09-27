f8f1f1ae065afe82f343653b41dd5c330d3a7ef8
Guryanov Vadim <vadim.guryanov.2004@mail.ru>
2022-09-27 12:40:38 +0300

    Гурьянов 2.1.3

---
+++ Homework3/Hw3.Tests/SingleInitializationSingleton.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Threading;
+
+namespace Hw3.Tests;
+
+public class SingleInitializationSingleton
+{
+    private static readonly object Locker = new();
+
+    private static volatile bool _isInitialized = false;
+
+    public const int DefaultDelay = 3_000;
+    
+    public int Delay { get; private set;}
+
+    private SingleInitializationSingleton(int delay = DefaultDelay)
+    {
+        Delay = delay;
+        // imitation of complex initialization logic
+        Thread.Sleep(delay);           
+    }
+
+    internal static void Reset()
+    {
+        if(_isInitialized)
+        {
+            lock(Locker)
+            {
+                if(_isInitialized)
+                {
+                    _isInitialized = false;
+                    instance = new Lazy<SingleInitializationSingleton>(() => new SingleInitializationSingleton());
+                }
+            }
+        }
+    }
+
+    public static void Initialize(int delay)
+    {
+        if(!_isInitialized)
+        {
+            lock(Locker)
+            {
+                if(!_isInitialized)
+                {
+                    _isInitialized = true;
+                    instance = new Lazy<SingleInitializationSingleton>(() => new SingleInitializationSingleton(delay));
+                }
+                else
+                    throw new InvalidOperationException("Double initialization!");
+            }
+        }
+        else
+            throw new InvalidOperationException("Double initialization!");
+    }
+
+    private static Lazy<SingleInitializationSingleton> instance = new Lazy<SingleInitializationSingleton>(() => new SingleInitializationSingleton());
+
+    public static SingleInitializationSingleton Instance
+    {
+        get
+        { 
+            lock(Locker)
+                return instance.Value; 
+        }
+    }    
+}
