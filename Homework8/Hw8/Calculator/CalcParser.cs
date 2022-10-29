5062fd2a398c9d164f28f5fa04ae3c7e2375b4a6
Guryanov Vadim <vadim.guryanov.2004@mail.ru>
2022-10-29 20:57:08 +0300

    Гурьянов 2.1.8

---
+++ Homework8/Hw8/Calculator/CalcParser.cs
@@ -0,0 +1,29 @@
+using System.Collections.ObjectModel;
+
+namespace Hw8.Calculator;
+
+public static class CalcParser
+{
+    public static void ParseCalcArguments(ReadOnlyCollection<string> args,
+        out double val1,
+        out Operation operation,
+        out double val2)
+    {
+        operation = ParseOperation(args[1]);
+
+        if (!double.TryParse(args[0], out val1) | !double.TryParse(args[2], out val2))
+            throw new ArgumentException(Messages.InvalidNumberMessage);
+    }
+
+    private static Operation ParseOperation(string operation)
+    {
+        return operation switch
+        {
+            "Plus" => Operation.Plus,
+            "Minus" => Operation.Minus,
+            "Multiply" => Operation.Multiply,
+            "Divide" => Operation.Divide,
+            _ => throw new InvalidOperationException(Messages.InvalidOperationMessage),
+        };
+    }
+}
