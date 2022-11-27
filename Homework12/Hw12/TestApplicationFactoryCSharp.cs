321b58cb65be85155528ff232f1d2ee635500c4f
Guryanov Vadim <vadim.guryanov.2004@mail.ru>
2022-11-27 14:51:50 +0300

    generics fixes

--- Homework12/Hw12/TestApplicationFactoryCSharp.cs
+++ Homework12/Hw12/TestApplicationFactoryCSharp.cs
@@ -2,13 +2,6 @@ using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Hw12;
 
-public class TestApplicationFactoryCSharp : WebApplicationFactory<TestApplicationFactoryCSharp>
-    // TODO: replace generic argument with the right one
+public class TestApplicationFactoryCSharp : WebApplicationFactory<Hw8.Program>
 {
-    public new HttpClient CreateClient()
-    {
-        var client = new HttpClient();
-        client.BaseAddress = new Uri("https://localhost:7008");
-        return client;
-    }
 }
--- Homework12/Hw12/TestApplicationFactoryFSharp.cs
+++ Homework12/Hw12/TestApplicationFactoryFSharp.cs
@@ -2,13 +2,6 @@ using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Hw12;
 
-public class TestApplicationFactoryFSharp : WebApplicationFactory<TestApplicationFactoryFSharp>
-    // TODO: replace generic argument with the right one
+public class TestApplicationFactoryFSharp : WebApplicationFactory<Hw6.App.Startup>
 {
-    public new HttpClient CreateClient()
-    {
-        var client = new HttpClient();
-        client.BaseAddress = new Uri("https://localhost:59943");
-        return client;
-    }
 }
