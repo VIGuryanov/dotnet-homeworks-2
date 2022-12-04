98934bcdbdc0f64ffa07aeb112514e132092cefa
Guryanov Vadim <vadim.guryanov.2004@mail.ru>
2022-12-04 15:29:58 +0300

    Added more tests

--- Homework10/Hw10.Tests/TestApplicationFactory.cs
+++ Homework10/Hw10.Tests/TestApplicationFactory.cs
@@ -4,7 +4,6 @@ using Hw10;
 using Hw10.DbModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Homework10.Tests;
@@ -15,13 +14,13 @@ public class TestApplicationFactory : WebApplicationFactory<Program>
     {
         builder.ConfigureServices(serviceCollection =>
         {
-            var dbDescriptor = serviceCollection.SingleOrDefault(
-                descriptor => descriptor.ServiceType == typeof(DbContextOptions<ApplicationContext>));
-            serviceCollection.Remove(dbDescriptor!);
+            /*var dbDescriptor = serviceCollection.SingleOrDefault(
+                descriptor => descriptor.ServiceType == typeof(DbContextOptions<ApplicationContext>));*/
+            //serviceCollection.Remove(dbDescriptor!);
             
             var nameTestDb = Guid.NewGuid().ToString();
-            serviceCollection.AddDbContext<ApplicationContext>(
-                optionsBuilder => optionsBuilder.UseInMemoryDatabase(nameTestDb));
+            /*serviceCollection.AddDbContext<ApplicationContext>(
+                optionsBuilder => optionsBuilder.UseInMemoryDatabase(nameTestDb));*/
         });
 
         base.ConfigureWebHost(builder);
